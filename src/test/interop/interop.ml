7f4cf55fe1cc7a363f296ef98fc1fa9208adcbac
Aymeric Fromherz <afromher@andrew.cmu.edu>
2018-02-19 14:32:55 -0500

    Interop: Vale prototypes

--- src/test/interop/interop.ml
+++ src/test/interop/interop.ml
@@ -41,11 +41,80 @@ let translate_lowstar func =
   "\n\t(requires (fun h -> " ^ (liveness "h" args) ^ separator ^ (disjoint args) ^ "))\n\t" ^
   "(ensures (fun h0 _ h1 -> " ^ (liveness "h0" args) ^ separator ^ (liveness "h1" args) ^ "))\n"
   
-let translate_vale os target func =
+let print_vale_bufferty = function
+  | TUInt8 -> "buffer8"
+
+let rec print_vale_args = function
+  | [] -> ""
+  | (a, ty)::q -> ", ghost " ^ a ^ ":" ^ print_vale_bufferty ty ^ print_vale_args q
+
+let rec print_vale_loc_buff = function
+  | [] -> ""
+  | [(a, _)] -> "loc_buffer("^a^")"
+  | (a, _)::q -> "loc_buffer("^a^"), " ^ print_vale_loc_buff q
+
+let rec print_buff_readable = function
+  | [] -> ""
+  | (a, _)::q -> "        buffer_readable(mem, "^a^");\n" ^ print_buff_readable q
+
+let print_calling_args os target args =
   match target with
   | X86 -> begin match os with
-    | Windows -> ""
-    | Linux -> ""
+    | Windows -> failwith "windows calling convention not supported"
+    | Linux -> begin match args with
+      | [] -> ""
+      | [(a, _)] -> "        rsi == buffer_addr("^a^");\n"
+      | [(a, _); (b, _)] -> 
+        "        rsi == buffer_addr("^a^");\n        rdi == buffer_addr("^b^");\n"
+      | [(a, _); (b, _); (c, _)] -> 
+        "        rsi == buffer_addr("^a^");\n        rdi == buffer_addr("^b^");\n" ^
+        "        rdx == buffer_addr("^c^");\n"
+      | [(a, _); (b, _); (c, _); (d, _)] -> 
+        "        rsi == buffer_addr("^a^");\n        rdi == buffer_addr("^b^");\n" ^
+        "        rdx == buffer_addr("^c^");\n        rcx == buffer_addr("^d^");\n"
+      | [(a, _); (b, _); (c, _); (d, _); (e, _)] -> 
+        "        rsi == buffer_addr("^a^");\n        rdi == buffer_addr("^b^");\n" ^
+        "        rdx == buffer_addr("^c^");\n        rcx == buffer_addr("^d^");\n" ^
+        "        r8 == buffer_addr("^e^");\n"
+      | [(a, _); (b, _); (c, _); (d, _); (e, _); (f, _)] ->
+        "        rsi == buffer_addr("^a^");\n        rdi == buffer_addr("^b^");\n" ^
+        "        rdx == buffer_addr("^c^");\n        rcx == buffer_addr("^d^");\n" ^
+        "        r8 == buffer_addr("^e^");\n        r9 == buffer_addr("^f^");\n"
+      | _ -> failwith "Linux x86 supports only up to 6 arguments in registers"
+    end
+  end  
+
+let print_vale_reads os target args =
+  match target with
+  | X86 -> begin match os with
+    | Windows -> failwith "windows calling convention not supported"
+    | Linux -> begin match args with
+      | [] -> failwith "no arguments passed"
+      | [_] -> "rsi;\n"
+      | [_; _] -> "rsi; rdi;\n"
+      | [_; _; _] -> "rsi; rdi; rdx;\n"
+      | [_; _; _; _] -> "rsi; rdi; rdx; rcx;\n"
+      | [_; _; _; _; _] -> "rsi; rdi; rdx; rcx; r8;\n"
+      | [_; _; _; _; _; _] -> "rsi; rdi; rdx; rcx; r8; r9;\n"
+      | _ -> failwith "Linux x86 only supports up to 6 arguments in registers"
+    end
   end
 
-let () = print_string (translate_lowstar ("memcpy", [("src", TUInt8); ("dest", TUInt8)]));;
+let translate_vale os target func =
+  let name, args = func in
+  "#verbatim interface implementation\nmodule "^(String.capitalize_ascii name) ^
+  "\nmodule M = Memory_i_s\nopen X64.Machine_s\nopen X64.Vale.State_i\nopen X64.Vale.Decls\n#set-options \"--z3rlimit 20\"\n#end verbatim\n\n" ^
+  "procedure " ^ (String.capitalize_ascii name) ^ "(inline t:taint" ^ print_vale_args args ^")\n" ^
+  "    requires/ensures\n" ^
+  "        locs_disjoint(list(" ^ print_vale_loc_buff args ^ "));\n" ^
+  print_buff_readable args ^
+  print_calling_args os target args ^
+  "    reads\n" ^
+  "        " ^ print_vale_reads os target args ^
+  "    modifies\n" ^
+  "        mem; memTaint; trace;\n"^
+  "{\n\n}\n"
+
+let () = 
+  let func = ("memcpy", [("src", TUInt8); ("dest", TUInt8)]) in
+print_string (translate_lowstar func); print_newline(); print_newline(); print_string (translate_vale Linux X86 func);;
