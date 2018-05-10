40b10e2d0d1820b3b0d5592cddaf5748bbe18893
Barry Bond <barrybo@microsoft.com>
2018-05-10 14:11:25 -0700

    Cygwin scons launcher that supports Ctrl+C when combined with the job object code.

---
+++ scons_cygwin.sh
@@ -0,0 +1,3 @@
+PYTHON=$(cygpath -u "C:\Program Files (x86)\Microsoft Visual Studio\Shared\Python36_64\python.exe")
+SCONS="C:\Program Files (x86)\Microsoft Visual Studio\Shared\Python36_64\Scripts\scons.py"
+"${PYTHON}" "${SCONS}" $*
