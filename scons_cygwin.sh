682faef4df9c59374647821c7d66ee01ddc59979
Dzomo the Everest Yak <everbld@microsoft.com>
2018-05-14 15:54:27 -0700

    try again

--- scons_cygwin.sh
+++ scons_cygwin.sh
@@ -1,3 +1,5 @@
-SCONS=$(cygpath -d "$(dirname `which scons.bat`)/scons.py")
-PYTHON=$(cygpath -d "$(dirname `which scons.bat`)/python.exe")
+SCONS=$(cygpath -d "$(dirname `which scons.bat`)/Scripts/scons.py")
+
+PYTHON=$(cygpath -u "$(dirname `which scons.bat`)/python.exe")
+
 "${PYTHON}" "${SCONS}" $*
