8482615f792d83787102db6bce6752d86e5e868a
Aymeric Fromherz <afromher@andrew.cmu.edu>
2018-07-09 16:49:59 -0400

    Add .h file for Kremlin extraction

---
+++ src/arch/x64/extracted/interop.h
@@ -0,0 +1,17 @@
+#ifndef __INTEROP_H
+#define __INTEROP_H
+
+#include "AESEncryptBE.h"
+#include "AESEncryptBlock.h"
+#include "Gcm_load_xor.h"
+#include "Gcm_make_length.h"
+#include "GCTR.h"
+#include "GHash_extra.h"
+#include "GHash_one_block.h"
+#include "GHash_stdcall.h"
+#include "Inc32.h"
+#include "Mk_quad32_1.h"
+#include "Quad32_xor.h"
+#include "Zero_quad32.h"
+
+#endif
