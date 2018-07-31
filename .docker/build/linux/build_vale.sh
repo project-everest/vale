51385b87d901a2a33facfde225c21a91e163e021
Gustavo Varo <gustavo.varo@microsoft.com>
2018-07-31 16:56:26 -0400

    fix build script by removing the step of fetching kremlin

--- .docker/build/linux/build_vale.sh
+++ .docker/build/linux/build_vale.sh
@@ -14,20 +14,6 @@ function export_home() {
     fi
 }
 
-# By default, kremlin master works against F* stable. Can also be overridden.
-function fetch_kremlin() {
-  if [ ! -d kremlin ]; then
-    git clone https://github.com/FStarLang/kremlin kremlin
-  fi
-  cd kremlin
-  git fetch origin
-  local ref=$( if [ -f ../.kremlin_version ]; then cat ../.kremlin_version | tr -d '\r\n'; else echo origin/master; fi )
-  echo Switching to KreMLin $ref
-  git reset --hard $ref
-  cd ..
-  export_home KREMLIN "$(pwd)/kremlin"
-}
-
 function exec_build () {
     cd vale
 
@@ -42,13 +28,11 @@ function exec_build () {
         return
     fi
 
-    fetch_kremlin
-
     # Set up build environment
     nuget restore tools/Vale/src/packages.config -PackagesDirectory tools/FsLexYacc
 
     if [[ $target == "vale-ci" ]]; then
-        echo target -> vale-ci
+        echo "target -> vale-ci"
         scons -j $threads --NOVERIFY && echo -n true > $status_file;
     else
         echo "Invalid target"
